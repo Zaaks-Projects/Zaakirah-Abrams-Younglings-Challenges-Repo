b82096c79938ae48c4470ac0643d580d461ccd94
Zaaks-Projects <82881610+Zaaks-Projects@users.noreply.github.com>
2021-05-28 11:54:12 +0200

    Add files via upload

--- School Site V2/src/action_page.php
+++ School Site V2/src/action_page.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['submit']))
 {
-    $secretKey = "6Ld8IfUaAAAAADvbcOACYun-YU7w6XJTQYuknDgF";
+    $secretKey = "6LclHPgaAAAAALUUax1RbIfxAuFqnP6s5h_q3F-W";
     $responsKey = $_POST['g-recaptcha-response'];
     $UserIP = $_SERVER['REMOTE_ADDR'];
     $url = "http://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responsKey&remoteip=$UserIP";
